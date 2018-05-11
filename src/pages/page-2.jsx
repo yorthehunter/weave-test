1963ee5dbaebcd11603be0cb8467a7e9f82d747c
yorthehunter <boru.design@gmail.com>
2018-05-11 17:01:04 -0400

    banner

--- src/pages/page-2.jsx
+++ src/pages/page-2.jsx
@@ -1,12 +1,58 @@
-import React from 'react'
-import Link from 'gatsby-link'
+import React from 'react';
+import Link from 'gatsby-link';
 
 const SecondPage = () => (
   <div>
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
     <Link to="/">Go back to the homepage</Link>
-  </div>
-)
 
-export default SecondPage
+    <div className="weave-banner">
+      <p className="weave-banner__message">Hello!</p>
+      <a className="weave-banner__close" />
+    </div>
+
+    <div className="weave-banner weave-banner--error">
+      <p className="weave-banner__message">Hello!</p>
+      <a className="weave-banner__close" />
+    </div>
+
+    <div className="weave-banner weave-banner--info">
+      <p className="weave-banner__message">Hello!</p>
+      <a className="weave-banner__close" />
+    </div>
+
+    <div className="weave-banner weave-banner--success">
+      <p className="weave-banner__message">Hello!</p>
+      <a className="weave-banner__close" />
+    </div>
+
+    <div className="weave-banner weave-banner--warning">
+      <p className="weave-banner__message">Hello!</p>
+      <a className="weave-banner__close" />
+    </div>
+
+
+    <div className="weave-banner weave-banner--sm">
+      <p className="weave-banner__message">Hello!</p>
+      <a className="weave-banner__close" />
+    </div>
+
+    <div className="weave-banner weave-banner--md">
+      <p className="weave-banner__message">Hello!</p>
+      <a className="weave-banner__close" />
+    </div>
+
+    <div className="weave-banner weave-banner--lg">
+      <p className="weave-banner__message">Hello!</p>
+      <a className="weave-banner__close" />
+    </div>
+
+    <div className="weave-banner weave-banner--responsive">
+      <p className="weave-banner__message">Hello!</p>
+      <a className="weave-banner__close" />
+    </div>
+  </div>
+);
+
+export default SecondPage;

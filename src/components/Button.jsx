8c5ab45f23ca64042b1fc77d96be3ea89490ea60
yorthehunter <boru.design@gmail.com>
2018-05-11 16:39:35 -0400

    button updates

---
+++ src/components/Button.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { node, oneOf, bool } from 'prop-types';
+import classNames from 'classnames';
+
+const Button = (props) => {
+  const {
+    type,
+    size,
+    responsive,
+    disabled,
+    fullWidth,
+    children,
+  } = props;
+
+  const componentClasses = classNames([
+    'weave-button',
+    size ? `weave-button--${size}` : '',
+    type ? `weave-button--${type}` : '',
+    responsive ? 'weave-button--responsive' : '',
+    fullWidth ? 'weave-button--full-width' : '',
+    disabled ? 'is-disabled' : '',
+  ]);
+
+  return (
+    <button className={componentClasses}>
+      {children}
+    </button>
+  );
+};
+
+Button.defaultProps = {
+  type: undefined,
+  size: undefined,
+  disabled: false,
+  responsive: false,
+  fullWidth: false,
+};
+
+Button.propTypes = {
+  type: oneOf(undefined, 'primary', 'secondary', 'cta', 'flat'),
+  size: oneOf(undefined, 'xs', 'sm', 'md', 'lg'),
+  disabled: bool,
+  responsive: bool,
+  fullWidth: bool,
+  children: node.isRequired,
+};
+
+export default Button;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Link from 'gatsby-link';
 import P from '../components/P';
+import Button from '../components/Button';
 
 const IndexPage = () => (
   <div>
@@ -8,6 +9,42 @@ const IndexPage = () => (
     <P>Welcome to your new Gatsby site.</P>
     <P>Now go build something great.</P>
     <Link to="/page-2/">Go to page 2</Link>
+
+    <div>
+      <Button size="xs">Button</Button>
+      <Button size="sm">Button</Button>
+      <Button size="md">Button</Button>
+      <Button size="lg">Button</Button>
+      <Button>Button</Button>
+    </div>
+
+    <div>
+      <Button fullWidth size="xs">Button</Button>
+      <Button fullWidth size="sm">Button</Button>
+      <Button fullWidth size="md">Button</Button>
+      <Button fullWidth size="lg">Button</Button>
+      <Button>Button</Button>
+    </div>
+
+    <div>
+      <Button type="primary">Button</Button>
+      <Button type="secondary">Button</Button>
+      <Button type="cta">Button</Button>
+      <Button type="flat">Button</Button>
+      <Button>Button</Button>
+    </div>
+
+    <div>
+      <Button responsive>Button</Button>
+    </div>
+
+    <div>
+      <Button type="primary" disabled>Button</Button>
+      <Button type="secondary" disabled>Button</Button>
+      <Button type="cta" disabled>Button</Button>
+      <Button type="flat" disabled>Button</Button>
+      <Button disabled>Button</Button>
+    </div>
   </div>
 );
 
